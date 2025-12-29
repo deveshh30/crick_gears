6a3e8f8a77a234b48f196b461bd4e29910c599bd
deveshh30 <devesh30gupta@gmail.com>
2025-12-30 00:10:23 +0530

    mongo-db connected

--- server/Server.js
+++ server/Server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config(); // <-- must be first line
+
 
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
@@ -9,8 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("YOUR_MONGODB_URL")
-  .then(() => console.log("MongoDB connected"));
+const mongoUrl = process.env.MONGODB_URL || "";
+if (mongoUrl) {
+  mongoose.connect(mongoUrl)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err.message));
+} else {
+  console.warn("MONGODB_URL not set; skipping DB connection (server will run in limited mode)");
+}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
